92dd06ce9131836416cd044798481245e3853b5f
HongJBeh <132732783+HongJBeh@users.noreply.github.com>
2023-05-11 21:15:55 +0800

    Add files via upload

---
+++ ComputerScienceSociety/eventDisplay.php
@@ -0,0 +1,28 @@
+<?php  
+    $hostname = "airasia-db.cpfvj1jupn9o.us-east-1.rds.amazonaws.com";
+    $username = "airasia";
+    $password = "airasia123";
+    $dbName = "Airasia";
+
+    $k = $_POST['id'];
+    $k = trim($k);
+
+    $con = mysqli_connect($hostname,$username,$password,$dbName);
+    $sql = "Select * from events where Event='{$k}'";
+    $res = mysqli_query($con,$sql);
+
+    while($rows = mysqli_fetch_array($res)){
+?>
+<tr>
+    <td> <?php echo $rows['EventID']; ?> </td>
+    <td> <?php echo $rows['Event']; ?> </td>
+    <td> <?php echo $rows['Venue']; ?> </td>
+    <td> <?php echo $rows['Day']; ?> </td>
+    <td> <?php echo $rows['Time']; ?> </td>
+    <td> <?php echo $rows['Description']; ?> </td>
+</tr>
+
+<?php
+    }
+    echo $sql;
+?>
